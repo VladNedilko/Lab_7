c0393df108f7dcebed0bd002bb382e48af337a01
VladNedilko <nedilko444466@gmail.com>
2022-11-15 09:54:15 +0200

    Create 1 conlose aps

---
+++ sum.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    float x,Y,Sum,k;
+    Sum=0;
+    cout<<"Введіть х ";
+    cin>>x;
+    cout<<"Введіть k ";
+    cin>>k;
+    for (int k=1;k<=10;k++){
+        Sum+=k;
+    }
+    Y=log(10)*x+2*Sum*k;
+    cout<<Y;
+    return 0;
+}
+
